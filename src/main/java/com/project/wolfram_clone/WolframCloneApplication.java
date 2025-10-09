2e66f32a2ccc16874766f4b44cada3ed5907212c
AdrianBadea23 <badeaadrian699@gmail.com>
2025-10-09 21:13:39 +0300

    cleanup

--- src/main/java/com/project/wolfram_clone/WolframCloneApplication.java
+++ src/main/java/com/project/wolfram_clone/WolframCloneApplication.java
@@ -1,7 +1,5 @@
 package com.project.wolfram_clone;
 
-import com.project.wolfram_clone.core.PostfixSolver;
-import com.project.wolfram_clone.core.ShuntingYard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/project/wolfram_clone/controller/EquationController.java
+++ src/main/java/com/project/wolfram_clone/controller/EquationController.java
@@ -2,10 +2,7 @@ package com.project.wolfram_clone.controller;
 
 import com.project.wolfram_clone.core.PostfixSolver;
 import com.project.wolfram_clone.core.ShuntingYard;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
